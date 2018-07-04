17cf27de04cfadbf07735b31569e4dc7ea5fefe1
Kintamani <tri.wicaksono69@gmail.com>
2018-07-04 13:42:52 +0700

    change App(view) UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,35 +22,40 @@
   
   {{-- <!-- iCheck --> --}}
   <link rel="stylesheet" href="{{asset('adminLTE/plugins/iCheck/square/blue.css')}}">
-
-  
 </head>
+
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    {{-- <a href="../../index2.html"><b>Admin</b>LTE</a> --}}
+  {{--  LOGO  --}}
+    {{--  <img src="{{asset('pulic/bla/bla/logo.png')}}" alt="">  --}}
+    <a href="{!!url('/')!!}"><b>Admin</b>LTE</a>
   </div>
   {{-- <!-- /.login-logo --> --}}
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="{{route('login')}}" method="post">
+      {{csrf_field()}}
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" class="form-control" placeholder="Email" name="email" value="{{old('email')}}" required autofocus>
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
+
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" name="password" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
+
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox"> Remember Me
+              <input type="checkbox" {{old('remember')? 'checked':''}}> Remember Me
             </label>
           </div>
         </div>
+
         {{-- <!-- /.col --> --}}
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
@@ -59,13 +64,14 @@
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
+    {{--  SOSICAL LOGIN  --}}
+    {{--  <div class="social-auth-links text-center">
       <p>- OR -</p>
       <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
         Google+</a>
-    </div>
+    </div>  --}}
     {{-- <!-- /.social-auth-links --> --}}
 
     <a href="#">I forgot my password</a><br>
