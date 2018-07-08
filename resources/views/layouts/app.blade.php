d6bfae3eeb661ea1ad64d07046127315d9feda9f
Kintamani <tri.wicaksono69@gmail.com>
2018-07-08 22:47:51 +0700

    correcting code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>{{config('app.name','Laravel')}}</title>
   {{--  <!-- Tell the browser to be responsive to screen width -->  --}}
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   {{--  <!-- Bootstrap 3.3.6 -->  --}}
@@ -17,36 +17,11 @@
   {{--  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect.-->--}}
-      <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="{{asset('adminLTE/dist/css/skins/skin-blue.min.css')}}">
 
-      {{--  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->  --}}
-      {{--  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->  --}}
-  {{--  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->  --}}
+      
 
 </head>
-{{--  <!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->  --}}
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
 
@@ -54,10 +29,10 @@ desired effect
     <header class="main-header">
 
       {{--  <!-- Logo -->  --}}
-      <a href="index2.html" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
+      <a href="#" class="logo">
+        {{-- <!-- mini logo for sidebar mini 50x50 pixels --> --}}
         <span class="logo-mini"><b>A</b>LT</span>
-        <!-- logo for regular state and mobile devices -->
+        {{-- <!-- logo for regular state and mobile devices --> --}}
         <span class="logo-lg"><b>Admin</b>LTE</span>
       </a>
 
@@ -87,7 +62,7 @@ desired effect
                       <a href="#">
                         <div class="pull-left">
                           {{--  <!-- User Image -->  --}}
-                          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                          <img src="{{asset('public/images'.Auth::user()->foto)}}" class="img-circle" alt="User Image">
                         </div>
                         {{--  <!-- Message title and timestamp -->  --}}
                         <h4>
@@ -173,42 +148,30 @@ desired effect
               {{--  <!-- Menu Toggle Button -->  --}}
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 {{--  <!-- The user image in the navbar-->  --}}
-                <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                <img src="{{asset('public/images'.Auth::user()->foto)}}" class="user-image" alt="User Image">
                 {{--  <!-- hidden-xs hides the username on small devices so only the image appears. -->  --}}
                 <span class="hidden-xs">Alexander Pierce</span>
               </a>
               <ul class="dropdown-menu">
                 {{--  <!-- The user image in the menu -->  --}}
                 <li class="user-header">
-                  <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                  <img src="{{asset('public/images'.Auth::user()->foto)}}" class="img-circle" alt="User Image">
 
                   <p>
-                    Alexander Pierce - Web Developer
+                    {{Auth::user()->name}}
                     <small>Member since Nov. 2012</small>
                   </p>
-                </li>
-                {{--  <!-- Menu Body -->  --}}
-                <li class="user-body">
-                  <div class="row">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </div>
-                  {{--  <!-- /.row -->  --}}
                 </li>
                 {{--  <!-- Menu Footer-->  --}}
                 <li class="user-footer">
                   <div class="pull-left">
-                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                    <a href="{{route('user.profil')}}" class="btn btn-default btn-flat">Profile</a>
                   </div>
                   <div class="pull-right">
-                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                    <a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-default btn-flat">Sign out</a>
+                    <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none">
+                      {{csrf_field()}}
+                    </form>
                   </div>
                 </li>
               </ul>
@@ -221,6 +184,8 @@ desired effect
         </div>
       </nav>
     </header>
+
+    
     {{--  <!-- Left side column. contains the logo and sidebar -->  --}}
     <aside class="main-sidebar">
 
@@ -230,7 +195,7 @@ desired effect
         {{--  <!-- Sidebar user panel (optional) -->  --}}
         <div class="user-panel">
           <div class="pull-left image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            <img src="{{asset('public/images'.Auth::user()->foto)}}" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
             <p>Alexander Pierce</p>
@@ -239,24 +204,29 @@ desired effect
           </div>
         </div>
 
-        {{--  <!-- search form (Optional) -->  --}}
-        <form action="#" method="get" class="sidebar-form">
-          <div class="input-group">
-            <input type="text" name="q" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-          </div>
-        </form>
-        {{--  <!-- /.search form -->  --}}
 
         {{--  <!-- Sidebar Menu -->  --}}
         <ul class="sidebar-menu" data-widget="tree">
           <li class="header">HEADER</li>
           {{--  <!-- Optionally, you can add icons to the links -->  --}}
-          <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-          <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+          <li><a href="{{route('home')}}"><i class="fa fa-dashboard"></i> <span>Dasboard</span></a></li>
+          @if (Auth::user->level==1)
+              <li><a href=""><i class="fa-fa-cube"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-cubes"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-credit"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-truck"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-money"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-user"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-upload"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-download"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-file-pdf-o"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-gears"><span>Kategori</span></i></li>
+          @else
+              <li><a href=""><i class="fa-fa-cube"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-cube"><span>Kategori</span></i></li>
+              <li><a href=""><i class="fa-fa-cube"><span>Kategori</span></i></li>
+          @endif
+          <li></li>
           <li class="treeview">
             <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
               <span class="pull-right-container">
